7f0a709d2cdfc50658e8e2f5f1f0f060d41400bf
Achref Bouadila <a_bouadila@live.fr>
2020-02-26 22:05:23 +0100

    create SoapConfig

---
+++ src/main/java/com/ashref/soap/api/config/SoapConfig.java
@@ -0,0 +1,40 @@
+package com.ashref.soap.api.config;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
+
+@Configuration
+@EnableWs
+public class SoapConfig {
+	@Bean
+	public ServletRegistrationBean<MessageDispatcherServlet> messageDispatcherServlet(ApplicationContext context) {
+		MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+		servlet.setApplicationContext(context);
+		servlet.setTransformWsdlLocations(true);
+		return new ServletRegistrationBean<MessageDispatcherServlet>(servlet, "/api/*");
+	}
+	
+	@Bean(name = "temperature")
+	public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema schema) {
+		DefaultWsdl11Definition defaultWsdl11Definition = new DefaultWsdl11Definition();
+		defaultWsdl11Definition.setPortTypeName("TemperatureIndicator");
+		defaultWsdl11Definition.setLocationUri("/api");
+		defaultWsdl11Definition.setTargetNamespace("http://www.ashref.com/soap/api/temperature");
+		defaultWsdl11Definition.setSchema(schema);
+		
+		return defaultWsdl11Definition;
+	}
+	
+	@Bean
+	public XsdSchema schema() {
+		return new SimpleXsdSchema(new ClassPathResource("xsd/temperature.xsd"));
+	}
+}
--- src/main/java/com/ashref/soap/api/endpoints/TemperatureEndPoint.java
+++ src/main/java/com/ashref/soap/api/endpoints/TemperatureEndPoint.java
@@ -1,13 +1,10 @@
 package com.ashref.soap.api.endpoints;
 
-import javax.xml.stream.events.Namespace;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
-
 import com.ashref.soap.api.services.TemperatureService;
 import com.ashref.soap.api.temperature.MinTemperatureRequest;
 import com.ashref.soap.api.temperature.MinTemperatureResponse;
@@ -17,7 +14,7 @@ public class TemperatureEndPoint {
 	@Autowired
 	private TemperatureService service;
 	private final String NAMESPACE = "http://www.ashref.com/soap/api/temperature";
-	
+
 	@PayloadRoot(namespace = NAMESPACE, localPart = "MinTemperatureRequest")
 	@ResponsePayload
 	public MinTemperatureResponse getMinTemperature(@RequestPayload MinTemperatureRequest request) {
