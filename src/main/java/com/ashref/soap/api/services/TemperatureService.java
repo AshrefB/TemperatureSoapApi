97007d8d690ada5740d9b1ee8be0367a175f5cc1
Achref Bouadila <a_bouadila@live.fr>
2020-02-26 22:34:09 +0100

    add avg method in TemplateService

--- src/main/java/com/ashref/soap/api/services/TemperatureService.java
+++ src/main/java/com/ashref/soap/api/services/TemperatureService.java
@@ -4,6 +4,8 @@ import java.util.Comparator;
 import java.util.List;
 import org.springframework.stereotype.Service;
 
+import com.ashref.soap.api.temperature.AvgTemperatureRequest;
+import com.ashref.soap.api.temperature.AvgTemperatureResponse;
 import com.ashref.soap.api.temperature.MaxTemperatureRequest;
 import com.ashref.soap.api.temperature.MaxTemperatureResponse;
 import com.ashref.soap.api.temperature.MinTemperatureRequest;
@@ -24,6 +26,18 @@ public class TemperatureService {
 		List<Integer> list = request.getTemperatures();
 		list.sort(Comparator.naturalOrder());
 		response.setResult(list.get(list.size()-1));
+		return response;
+	}
+	
+	public AvgTemperatureResponse avg(AvgTemperatureRequest request) {
+		AvgTemperatureResponse response = new AvgTemperatureResponse();
+		List<Integer> list = request.getTemperatures();
+		double avg = list.stream()
+			.mapToInt(i -> i)
+			.average()
+			.getAsDouble();
+		
+		response.setResult(avg);
 		return response;
 	}
 }
