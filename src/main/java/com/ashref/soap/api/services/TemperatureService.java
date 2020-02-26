6af40c31f1f08ed530fc463b571875c4051f2b63
Achref Bouadila <a_bouadila@live.fr>
2020-02-26 22:18:44 +0100

    add max method in TemplateService

--- src/main/java/com/ashref/soap/api/services/TemperatureService.java
+++ src/main/java/com/ashref/soap/api/services/TemperatureService.java
@@ -3,6 +3,9 @@ package com.ashref.soap.api.services;
 import java.util.Comparator;
 import java.util.List;
 import org.springframework.stereotype.Service;
+
+import com.ashref.soap.api.temperature.MaxTemperatureRequest;
+import com.ashref.soap.api.temperature.MaxTemperatureResponse;
 import com.ashref.soap.api.temperature.MinTemperatureRequest;
 import com.ashref.soap.api.temperature.MinTemperatureResponse;
 
@@ -13,6 +16,14 @@ public class TemperatureService {
 		List<Integer> list = request.getTemperatures();
 		list.sort(Comparator.naturalOrder());
 		response.setResult(list.get(0));
+		return response;
+	}
+	
+	public MaxTemperatureResponse max(MaxTemperatureRequest request) {
+		MaxTemperatureResponse response = new MaxTemperatureResponse();
+		List<Integer> list = request.getTemperatures();
+		list.sort(Comparator.naturalOrder());
+		response.setResult(list.get(list.size()-1));
 		return response;
 	}
 }
